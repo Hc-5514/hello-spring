690ac9b24be1e0deb40abad28cb1306916512b57
HC-5514 <tbd05050@naver.com>
2022-08-05 17:33:25 +0900

    강의 목차: 스프링 DB 접근 기술 JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id 값을 자동으로 생성
     private Long id;
+
+//    @Column(name = "username")
     private String name;
 
     public Long getId() {
